40953f1dc57f6b6c98def4cbf1dc91b9f3955da7
ch3rk4 <pety.shurupov@gmail.com>
2025-07-19 19:40:30 +0300

    create serializers

---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+
+User = get_user_model()
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    """Сериализатор для создания пользователя."""
+
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'email', 'username', 'password', 'telegram_chat_id')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'telegram_chat_id')
+        read_only_fields = ('id',)
