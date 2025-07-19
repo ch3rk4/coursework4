005637f2462c7871decf66f252e32b0c3e5dd07d
ch3rk4 <pety.shurupov@gmail.com>
2025-07-19 19:37:36 +0300

    create models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    telegram_chat_id = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Telegram Chat ID'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
