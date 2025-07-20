a5e95f83d68a6966464d226fd45fbaaa834a617f
ch3rk4 <pety.shurupov@gmail.com>
2025-07-20 11:55:39 +0300

    create admin from habits

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Habit
 
-# Register your models here.
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """Админка для привычек."""
+
+    list_display = (
+        'action', 'user', 'place', 'time',
+        'is_pleasant', 'is_public', 'created_at'
+    )
+    list_filter = ('is_pleasant', 'is_public', 'created_at')
+    search_fields = ('action', 'place', 'user__username')
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('user', 'action', 'place', 'time')
+        }),
+        ('Настройки', {
+            'fields': ('is_pleasant', 'is_public', 'periodicity', 'execution_time')
+        }),
+        ('Связи и награды', {
+            'fields': ('related_habit', 'reward')
+        }),
+        ('Метаданные', {
+            'fields': ('created_at',)
+        }),
+    )
