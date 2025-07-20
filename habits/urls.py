453bbf8f09d1afe58f3ce8fe3417f250e6cfab70
ch3rk4 <pety.shurupov@gmail.com>
2025-07-20 11:13:20 +0300

    create urls

---
+++ habits/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    HabitListCreateView,
+    HabitDetailView,
+    PublicHabitListView
+)
+
+app_name = 'habits'
+
+urlpatterns = [
+    path('habits/', HabitListCreateView.as_view(), name='habit-list-create'),
+    path('habits/<int:pk>/', HabitDetailView.as_view(), name='habit-detail'),
+    path('habits/public/', PublicHabitListView.as_view(), name='public-habits'),
+]
